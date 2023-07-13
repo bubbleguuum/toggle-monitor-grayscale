a83735ce6208bc86d355ea9d9eeb828045b338a1
bubbleguuum <bubbleguuum@free.fr>
2023-07-13 12:25:59 +0200

    picom experimental shader: apply grayscale after default post processing

--- toggle-monitor-grayscale.sh
+++ toggle-monitor-grayscale.sh
@@ -94,10 +94,10 @@ uniform float opacity;
 vec4 default_post_processing(vec4 c);
 
 vec4 window_shader() {
-    vec4 c = texelFetch(tex, ivec2(texcoord), 0);
+    vec4 c = default_post_processing(texelFetch(tex, ivec2(texcoord), 0));
     float y = dot(c.rgb, vec3(0.2126, 0.7152, 0.0722));
     c = opacity*vec4(y, y, y, c.a);
-    return default_post_processing(c);
+    return c;
 }
 EOF
 	    
